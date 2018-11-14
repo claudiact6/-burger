c6c2969f0f40a30640de5b2c3a3a30cc32ebc7d9
kimk <38691828+claudiact6@users.noreply.github.com>
2018-11-13 20:34:02 -0600

    show input field when clicking on burger type

--- public/assets/js/burgeractions.js
+++ public/assets/js/burgeractions.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $("#add").on("click", function() {
+$(document).ready(function () {
+    $("#add").on("click", function () {
         event.preventDefault();
         console.log("clicked");
         var burger = $("#burgertype").val();
@@ -8,9 +8,32 @@ $(document).ready(function() {
             data: {
                 burger_name: burger
             }
-        }).then(function() {
+        }).then(function () {
             location.reload();
         });
     });
 
+    $(document).on("click", ".burger", editBurger);
+    $(document).on("blur", ".burger", cancelEdit);
+
 });
+
+function editBurger() {
+    console.log("burger clicked");
+    var currentBurger = $(this).text();
+    console.log(currentBurger);
+    $(this).children().hide();
+    $(this).children("input.edit").val(currentBurger.text);
+    $(this).children("input.edit").show();
+    $(this).children("input.edit").focus();
+};
+
+function cancelEdit() {
+    var currentBurger = $(this).text();
+    console.log("canceling edit", currentBurger);
+    if (currentBurger) {
+      $(this).children().hide();
+      $(this).children("input.edit").val(currentBurger.text);
+      $(this).children("span").show();
+    }
+};
