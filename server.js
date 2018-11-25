8698ccc3b542888f6141daf29f9f109d7bd9e461
kimk <38691828+claudiact6@users.noreply.github.com>
2018-11-25 11:09:31 -0600

    trying stuff

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-var bodyParser = require("body-parser");
-
 app.use(express.static(__dirname + "/public"));
 
 //parsing
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //Set up Handlebars
