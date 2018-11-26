54ea74357331bd800f44e562ddff0f8b12006ca3
kimk <38691828+claudiact6@users.noreply.github.com>
2018-11-25 20:09:42 -0600

    fixed connection file for heroku deploy

--- config/orm.js
+++ config/orm.js
@@ -4,21 +4,21 @@ var orm = {
     selectAll: function(table, cb) {
         var queryString = "SELECT * FROM ??";
         connection.query(queryString, table, function(err, data) {
-            //if (err) throw err;
+            if (err) throw err;
             cb(data);
         });
     },
     insertOne: function(table, column, value, cb) {
         var queryString = "INSERT INTO ?? (??) VALUES (?)";
         connection.query(queryString, [table, column, value], function(err, data) {
-            //if (err) throw err;
+            if (err) throw err;
             cb(data);
         });
     },
     updateOne: function(table, col1, val1, col2, val2) {
         var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         connection.query(queryString, [table, col1, val1, col2, val2], function(err, data) {
-            //if (err) throw err;
+            if (err) throw err;
             cb(data);
         });
     }
